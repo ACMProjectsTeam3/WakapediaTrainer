497a3d2737f2f749e33bb840a09213978826f469
Arpakasso <etrinh96@gmail.com>
2017-03-28 22:21:15 -0500

    Updated GenerateCatergoryMC.py

--- GenerateCategoryMC.py
+++ GenerateCategoryMC.py
@@ -7,21 +7,25 @@ This gets links to wikipedia pages from a category page.
 Returns a list.
 """
 
-def scrape_category_page(url):
-    links = []
+def scrape_category_page(url, links):
     soup = BeautifulSoup(urllib.request.urlopen(url), 'lxml')
-
-      ### accounts for categories with over 200 pages
-    link = soup.find('a', href=True, text='next page')
-    if (link != None):
-        links += scrape_category_page('https://en.wikipedia.org' + link['href'])
 
       ### sends links of wikipedia articles in the category to be scraped
     pages_in_category = soup.find('div', {'id':'mw-pages'}).find('div',{'class':'mw-category'})
-    for obj in pages_in_category.findAll('a'):
-        links.append('https://en.wikipedia.org' + obj['href'])
-    return links
 
+    if (pages_in_category != None):
+        for obj in pages_in_category.findAll('a'):
+            if (len(links) < 500):
+                links.append('https://en.wikipedia.org' + obj['href'])
+                print (str(len(links)) + obj['href'])
+            else:
+                break
+
+       ### accounts for categories with over 200 pages
+    link = soup.find('a', href=True, text='next page')
+    if (link != None and len(links) < 500):
+        scrape_category_page('https://en.wikipedia.org' + link['href'], links)
+    
 
 """
 This gets the texts in the paragraphs of a webpage.
@@ -29,7 +33,11 @@ Returns a string
 """
 def scrape(url):
       ### opens url so it's like a file
-    link = urllib.request.urlopen(url)
+    try: 
+        link = urllib.request.urlopen(url)
+    except urllib.error.HTTPError:
+        return ''
+        
     soup = BeautifulSoup(link.read().decode('utf-8'), 'lxml', parse_only=SoupStrainer('p'))
 
     alltxt = ''
@@ -45,24 +53,28 @@ Scripty script
 
 
 CATEGORIES = set()
-while (len(CATEGORIES) < 1):
-    link = urllib.request.urlopen('https://en.wikipedia.org/wiki/Adolf_Hitler')
-    soup = BeautifulSoup(link, 'lxml')
+while (len(CATEGORIES) < 10):
+    url = urllib.request.urlopen('https://en.wikipedia.org/wiki/Special:Random')
+    soup = BeautifulSoup(url, 'lxml')
 
+    print (soup.title.string)
+
+	### get categories in the random wiki page
     for cat in soup.find('div', {'id': 'catlinks'}).find('ul').findAll('li'):
+	   ### markov chain it if we haven't already
         if cat.string not in CATEGORIES:
             cattext = ''
-            for link in scrape_category_page('https://en.wikipedia.org' + cat.find('a')['href']):
+            links = []
+            scrape_category_page('https://en.wikipedia.org' + cat.find('a')['href'], links)
+            for link in links:
                 cattext = cattext + "\n" + scrape(link)
             print (cat.string)
             TrainAndSaveString(cattext, './Categories/' + cat.string + '.mc')
-            
             #filename = './Categories/' + cat.string + '.txt'
             #os.makedirs(os.path.dirname(filename), exist_ok=True)
             #with open(filename, 'w') as file:
             #    file.write(cattext)
             
             CATEGORIES.add(cat.string)
-
 
 print (CATEGORIES)
