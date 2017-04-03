4d9f3fd77cbf8c67940ab96b4b615f40f5e4ef4b
Arpakasso <etrinh96@gmail.com>
2017-04-03 10:27:33 -0500

    This may break things

--- MarkovMaker.py
+++ MarkovMaker.py
@@ -6,60 +6,43 @@ import language_check
 import random
 import json
 import os
-
-
-class POSifiedText(markovify.Text):
-    def word_split(self, sentence):
-        words = re.split(self.word_split_pattern, sentence)
-        words = [ "::".join(tag) for tag in nltk.pos_tag(words) ]
-        return words
-    def word_join(self, words):
-        sentence = " ".join(word.split("::")[0] for word in words)
-        return sentence
-    
-
-class EditedTextClass(POSifiedText):
-    def __init__(self, input_text, state_size=2, chain=None, runs=None):
-        self.input_text = input_text
-        self.state_size = state_size
-        self.runs = runs or list(self.generate_corpus(self.input_text))
-        self.rejoined_text = self.sentence_join(map(self.word_join, self.runs))
-        self.chain = chain or markovify.Chain(self.runs, state_size)
-    def to_dict(self):
-        return {
-            "input_text": self.input_text,
-            "state_size": self.state_size,
-            "chain": self.chain.to_json(),
-            "runs": self.runs
-        }
-    @classmethod
-    def from_dict(cls, obj):
-        return cls(
-            obj["input_text"],
-            state_size=obj["state_size"],
-            chain=markovify.Chain.from_json(obj["chain"]),
-            runs=obj["runs"]
-        )
+import random
+from random import randint
 
 
 
-def CreateSentences(FILE_PATH_OF_OLDSTUFF, NUMSENTENCES): #definition to generate text. First parameter is the file-path to the .txt file you'll be using to train the model, the second parameter is how many sentences you want out of the markov model.
-	with open(FILE_PATH_OF_OLDSTUFF) as json_file:  
-		model2_json = json.load(json_file)
-	NEW_MODEL = EditedTextClass.from_json(model2_json)
+def CreateSentences(Bundle): #definition to generate text. Firstword = word.replace(letter,"!") parameter is the file-path to the .txt file you'll be using to train the model, the second parameter is how many sentences you want out of the markov model.
+	FILE_PATH_OF_OLDSTUFF = None
+	NEW_MODEL = markovify.Text("", state_size = 2)
+	for x in Bundle.categories:
+		FILE_PATH_OF_OLDSTUFF = x
+		FILE_PATH_OF_OLDSTUFF = FILE_PATH_OF_OLDSTUFF[FILE_PATH_OF_OLDSTUFF.find("Category:") + 9:]
+		FILE_PATH_OF_OLDSTUFF = "Categories/%s.mc" % FILE_PATH_OF_OLDSTUFF
+		if not os.path.isfile(FILE_PATH_OF_OLDSTUFF):
+			continue
+		for letter in FILE_PATH_OF_OLDSTUFF:
+			if letter == '_':
+				FILE_PATH_OF_OLDSTUFF = FILE_PATH_OF_OLDSTUFF.replace(letter, ' ')
+		with open(FILE_PATH_OF_OLDSTUFF) as json_file:
+			model2_json = json.load(json_file)
+		OLD_MODEL = markovify.Text.from_json(model2_json)
+		NEW_MODEL = markovify.combine([OLD_MODEL, NEW_MODEL], [1,1])
 	tool = language_check.LanguageTool('en-GB')
-	text = ""
-	for i in range(NUMSENTENCES): #creates 'NUMSENTENCES' sentence, where NUMSENTENCES is an integer
-		text = NEW_MODEL.make_sentence(tries = 1) #this, along with the next while loop, basically just forces the markov model to try an infinite number of times to have SOMETHING come out. 
-		while (text == None): 
-			text = NEW_MODEL.make_sentence(tries = 1)
-		matches = tool.check(text) #checks the grammar of the generated text
-		text = language_check.correct(text, matches) #corrects any mistakes the grammar checker found in the text
-		print (" ", end = "")
-		print (text, end="") #prints text. The 'end' is just there to ensure that no new line is created
-		NewLine = random.randint(0,30) #there is a 10% chance that a new line is created every time a sentence is created. This just makes the generated text not a huge block, and a little more natural looking 
-		if NewLine == 1 or NewLine == 2 or NewLine == 3:
-			print ("\n")
+	Text = ""
+	paragraphText = ""
+	for key in Bundle.paragraphs.keys():
+		NumberOfSentences = random.randint(1,12)
+		for i in range(NumberOfSentences): #creates 'NUMSENTENCES' sentence, where NUMSENTENCES is an integer
+			Text = NEW_MODEL.make_sentence(tries = 500) #this, along with the next while loop, basically just forces the markov model to try an infinite number of times to have SOMETHING come out.
+			if (Text == None):
+				continue
+			matches = tool.check(Text) #checks the grammar of the generated text
+			paragraphText += language_check.correct(Text, matches) #corrects any mistakes the grammar checker found in the text
+			paragraphText += " "
+		Bundle.paragraphs[key] = paragraphText
+		paragraphText = ""
+	return Bundle
+
 
 
 
@@ -68,7 +51,7 @@ def TrainAndSaveString(String, name):
 	os.makedirs(os.path.dirname(FILENAME), exist_ok=True)
 	corpus = String
 	corpus = re.sub( '\s+', ' ', corpus ).strip()
-	text_model = EditedTextClass(corpus, state_size=3, chain=None)
+	text_model = markovify.Text(corpus, state_size=2, chain=None)
 	model1_json = text_model.to_json()
-	with open(FILENAME, 'w') as outfile:  
+	with open(FILENAME, 'w') as outfile:
 	    json.dump(model1_json, outfile)
